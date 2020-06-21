fbe2be72a78759d87e34d5a899a95d6c3432ae3b
Warmachine13 <lucaspereiradesouzat@gmail.com>
2020-06-21 19:25:11 -0300

    ajustes

--- src/Validators/Users.go
+++
@@ -1,6 +0,0 @@
-package validatores
-
-type Query struct {
-	Page        uint64 `form:"user" json:"user" xml:"user"  binding:"required"`
-	RowsPerPage uint64 `form:"password" json:"password" xml:"password" binding:"required"`
-}
